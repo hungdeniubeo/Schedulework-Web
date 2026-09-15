ac9169aadb60e86f8ecc5a13e69cfdd7fd7fe423
hung <120816135+hungdeniubeo@users.noreply.github.com>
2026-09-15 17:10:19 +0700

    style: load scheduler parity extras

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./app/App";
 import "./styles/index.css";
 import "./styles/schedule.css";
 import "./scheduling/ScheduleExport.css";
+import "./admin/SchedulePortExtras.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
