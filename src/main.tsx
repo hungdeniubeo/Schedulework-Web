40d6791d4653e281388797a4455b73b604023d06
hung <120816135+hungdeniubeo@users.noreply.github.com>
2026-09-17 17:15:26 +0700

    feat: load mobile registration overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app/App";
 import "./styles/index.css";
+import "./styles/mobile-registration.css";
 import "./styles/schedule.css";
 import "./scheduling/ScheduleExport.css";
 import "./admin/SchedulePortExtras.css";
